84cd65cbfa5ec86774ff7d324f75dfa0720fdf5b
xinnieworkbrench <xinniemai989@gmail.com>
2025-03-18 23:17:20 -0400

    M09 challenge.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/xinnieworkbrench/m09-demo-2025',
+    author='Xinnie Mai',
+    author_email='rkc7gr@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
